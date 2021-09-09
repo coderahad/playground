65b03682cd909bf06b099a43c4e04c17784330d9
Ahad Hossain <hossain1814@gmail.com>
2021-09-09 20:19:41 +0600

    TH Aura Basic.4th cmt.Lesson-4.Function Chaining, Rewiring.

--- force-app/main/default/aura/helloMessageInteractive/helloMessageInteractiveController.js
+++ force-app/main/default/aura/helloMessageInteractive/helloMessageInteractiveController.js
@@ -3,5 +3,12 @@
         var btnClicked = event.getSource(); // the button
         var btnMessage = btnClicked.get("v.label"); // the button's label
         component.set("v.message", btnMessage);
+    },
+    handleClick2: function(component, event, helper) {
+        let newMessage = event.getSource().get("v.label");
+        component.set("v.message", newMessage);
+    },
+    handleClick3: function(component, event, helper) {
+        component.set("v.message", event.getSource().get("v.label"));
     }
 })
