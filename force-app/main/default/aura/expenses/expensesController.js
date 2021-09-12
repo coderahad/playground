9e78f8af1658f2affba78f75fe131a53ca1d737b
Ahad Hossain <hossain1814@gmail.com>
2021-09-12 20:09:46 +0600

    TH Aura Basic.7th cmt.Lesson-5.create clickCreate controller handler.

--- force-app/main/default/aura/expenses/expensesController.js
+++ force-app/main/default/aura/expenses/expensesController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
-
+    clickCreate : function(component, event, helper) {
+        let validExpense = component.find('expenseform').reduce(function(validSoFar, inputCmp) {
+            // DIsplay error message for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            console.log(inputCmp.get('v.validity', 'v.name'));
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // if we pass error checking, do some real work
+        if(validExpense) {
+            //create new expense
+            let newExpense = component.get("v.newExpense");
+            console.log("Create Expense: " + JSON.stringify(newExpense));
+            helper.createExpense(component, newExpense);
+        }
     }
 })
