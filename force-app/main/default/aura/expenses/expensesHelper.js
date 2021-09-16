b715ed0f0e55ca8a0543a0903a88adcbd350ded7
Ahad Hossain <hossain1814@gmail.com>
2021-09-16 17:55:18 +0600

    TH Aura Basic.8th cmt.Lesson-5.create createExpense helper handler.

--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,5 +1,10 @@
 ({
-    helperMethod : function() {
-
+    createExpense : function(component, expense) {
+        let theExpenses = component.get("v.expenses");
+        // copy the expense to a new object
+        // this is a disgusting, temporary hack
+        let newExpense = JSON.parse(JSON.stringify(expense));
+        theExpenses.push(newExpense);
+        component.set("v.expenses", tneExpenses);
     }
 })
