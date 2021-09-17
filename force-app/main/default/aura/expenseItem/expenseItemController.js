f78ae4e5ad53ad70221959f21567cb9ded0a3a5c
Ahad Hossain <hossain1814@gmail.com>
2021-09-17 11:41:54 +0600

    TH Aura Basic.10th cmt.Lesson-5.rewrite the expenseItem cmp to display the list of expenses.

--- force-app/main/default/aura/expenseItem/expenseItemController.js
+++ force-app/main/default/aura/expenseItem/expenseItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+        let mydate = component.get("v.expense.Date__c");
+        if(mydate){
+            component.set("v.formatdate", new Date(mydate));
+        }
+    },
 })
--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -6,7 +6,7 @@
         let newExpense = JSON.parse(JSON.stringify(expense));
         console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
         theExpenses.push(newExpense);
-        component.set("v.expenses", tneExpenses);
+        component.set("v.expenses", theExpenses);
         console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
     }
 })
