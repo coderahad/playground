ed9e9fee8f5f91c3f783132822b7e4735c819d81
Ahad Hossain <hossain1814@gmail.com>
2021-09-25 13:14:46 +0600

    TH Aura Basic.13th cmt.Lesson-6.Connect to salesforce with server side controllers.Wiring component and client side controller to the server-side apex controller.Call server side apex asynchronously (set a call back function) from the client side controller.

--- force-app/main/default/aura/expenses/expensesController.js
+++ force-app/main/default/aura/expenses/expensesController.js
@@ -13,5 +13,22 @@
             console.log("Create Expense: " + JSON.stringify(newExpense));
             helper.createExpense(component, newExpense);
         }
+    },
+    // LOad expenses from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        let action = component.get("c.getExpenses");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response){
+            let stat = response.getState();
+            if(stat === 'SUCCESS') {
+                component.set("v.expenses", response.getReturnValue());
+            }
+            else {
+                console.log('Failed with state: ' + state);
+            }
+        });
+        // send action off to be executed
+        $A.enqueueAction(action);
     }
 })
