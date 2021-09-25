7cc4f07b5c28001c0e35d26f9d1c40301268b971
Ahad Hossain <hossain1814@gmail.com>
2021-09-25 17:14:44 +0600

    TH Aura Basic.14th cmt.Lesson-6.Create new expense and save it to SF.Then render it to component.

--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,12 +1,19 @@
 ({
     createExpense : function(component, expense) {
-        let theExpenses = component.get("v.expenses");
-        // copy the expense to a new object
-        // this is a disgusting, temporary hack
-        let newExpense = JSON.parse(JSON.stringify(expense));
-        console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
-        theExpenses.push(newExpense);
-        component.set("v.expenses", theExpenses);
-        console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
+        let action = component.get("c.saveExpense");
+        action.setParams({
+            "expense": expense
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if(state === 'SUCCESS') {
+                let expenses = component.get("v.expenses");
+                expenses.push(response.getReturnValue());
+                // Here the expense attribute updated only in the component. It is not called all records again from the server. 
+                //the attribute get the records initially when the component loads with the help of doInit function.
+                component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
